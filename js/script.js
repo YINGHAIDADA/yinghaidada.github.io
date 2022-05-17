d4fc84b8161ce631fbd011e5f14f2763dca78f1a
YINGHAIDADA <pyj2897022134@gmail.com>
2022-05-17 17:21:12 +0800

    更改打字时间

--- js/script.js
+++ js/script.js
@@ -9,8 +9,8 @@ Licence (CC BY-NC-SA 4.0) http://creativecommons.org/licenses/by-nc-sa/4.0/
 setTimeout(function(){
     hit1 = document.getElementById("hit1")
     hit1.classList.toggle("typewriter")
-},3500);
+},2000);
 setTimeout(function(){
     hit2 = document.getElementById("hit2")
     hit2.style.display="block";
-},3500);
+},2000);
