8f9a512bd06ee6ef52220361b7e1ac2f6d944a00
YINGHAI <49584621+YINGHAIDADA@users.noreply.github.com>
2022-05-17 16:47:12 +0800

    add web
    files

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+@author Alberto Hartzet 
+*
+*I wouldn't mind if you use this piece of code in your project as long 
+as you give credit with a link to my site. www.albertohartzet.com
+*
+Licence (CC BY-NC-SA 4.0) http://creativecommons.org/licenses/by-nc-sa/4.0/
+*/
+setTimeout(function(){
+    hit1 = document.getElementById("hit1")
+    hit1.classList.toggle("typewriter")
+},3500);
+setTimeout(function(){
+    hit2 = document.getElementById("hit2")
+    hit2.style.display="block";
+},3500);
