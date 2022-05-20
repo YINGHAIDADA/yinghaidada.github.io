2a36641fa15821e78503b59522c43f58f322ef7a
YINGHAIDADA <pyj2897022134@gmail.com>
2022-05-20 20:47:01 +0800

    更改画廊样式，增加艺术图片

--- js/script.js
+++ js/script.js
@@ -6,6 +6,14 @@ as you give credit with a link to my site. www.albertohartzet.com
 *
 Licence (CC BY-NC-SA 4.0) http://creativecommons.org/licenses/by-nc-sa/4.0/
 */
+$(document).ready(function() {
+    $(".option").click(function(){
+        console.log("asd");
+        $(".option").removeClass("active");
+        $(this).addClass("active");
+     });
+});
+
 setTimeout(function(){
     hit1 = document.getElementById("hit1")
     hit1.classList.toggle("typewriter")
@@ -14,3 +22,4 @@ setTimeout(function(){
     hit2 = document.getElementById("hit2")
     hit2.style.display="block";
 },1500);
+
